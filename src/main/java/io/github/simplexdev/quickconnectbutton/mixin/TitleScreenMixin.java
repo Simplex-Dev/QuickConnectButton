33c9c0608ada5a9ce677c98a141cc4f74ea0368f
redcreeper14385 <mounderfod@gmail.com>
2021-06-07 18:41:39 +0100

    Add 1.16 support

--- src/main/java/io/github/simplexdev/quickconnectbutton/mixin/TitleScreenMixin.java
+++ src/main/java/io/github/simplexdev/quickconnectbutton/mixin/TitleScreenMixin.java
@@ -5,12 +5,13 @@ import me.shedaniel.autoconfig.AutoConfig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.gui.screen.ConnectScreen;
+import net.minecraft.client.gui.screen.DirectConnectScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.network.ServerAddress;
 import net.minecraft.client.network.ServerInfo;
 import net.minecraft.client.resource.language.I18n;
+import net.minecraft.network.ServerAddress;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
@@ -30,8 +31,8 @@ public class TitleScreenMixin extends Screen {
 
     @Inject(method="initWidgetsNormal", at = @At(value = "TAIL"))
     private void addCustomButton(int y, int spacingY, CallbackInfo ci) {
-        this.addDrawableChild(new ButtonWidget(this.width / 2 - 100 + 205, y, 50, 20, new LiteralText("Connect"), (buttonWidget) -> {
-            ConnectScreen.connect(this, this.client, ServerAddress.parse(config.address), new ServerInfo(I18n.translate("selectServer.defaultName"), config.address, false));
+        this.addButton(new ButtonWidget(this.width / 2 - 100 + 205, y + spacingY, 50, 20, new LiteralText("Connect"), (buttonWidget) -> {
+            this.client.openScreen(new ConnectScreen(this, this.client, new ServerInfo(I18n.translate("selectServer.defaultName"), config.address, false)));
         }));
     }
 }

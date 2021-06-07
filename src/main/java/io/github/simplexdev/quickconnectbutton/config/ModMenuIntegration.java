572c983ca5c3b68e60bbb43767c5a45513627260
Grayray <69988482+Grayray75@users.noreply.github.com>
2021-06-07 20:29:46 +0200

    Make config accessible via ModMenu

---
+++ src/main/java/io/github/simplexdev/quickconnectbutton/config/ModMenuIntegration.java
@@ -0,0 +1,12 @@
+package io.github.simplexdev.quickconnectbutton.config;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import me.shedaniel.autoconfig.AutoConfig;
+
+public class ModMenuIntegration implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> AutoConfig.getConfigScreen(QuickConnectButtonConfig.class, parent).get();
+    }
+}

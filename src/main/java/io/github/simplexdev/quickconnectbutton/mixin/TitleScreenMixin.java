c717c5275a68d534e242ea7f5cb52cdab822eca4
mounderfod <mounderfod@gmail.com>
2025-12-23 12:58:51 +0000

    Update to 1.21.1

--- src/main/java/io/github/simplexdev/quickconnectbutton/mixin/TitleScreenMixin.java
+++ src/main/java/io/github/simplexdev/quickconnectbutton/mixin/TitleScreenMixin.java
@@ -4,18 +4,20 @@ import io.github.simplexdev.quickconnectbutton.config.QuickConnectButtonConfig;
 import me.shedaniel.autoconfig.AutoConfig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.gui.screen.ConnectScreen;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.screen.TitleScreen;
-import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.network.ServerAddress;
-import net.minecraft.client.network.ServerInfo;
-import net.minecraft.client.resource.language.I18n;
-import net.minecraft.text.Text;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.ComponentRenderUtils;
+import net.minecraft.client.gui.screens.ConnectScreen;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.TitleScreen;
+import net.minecraft.client.multiplayer.ServerData;
+import net.minecraft.client.multiplayer.resolver.ServerAddress;
+import net.minecraft.client.resources.language.I18n;
+import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Environment(EnvType.CLIENT)
 @Mixin(TitleScreen.class)
@@ -23,35 +25,34 @@ public class TitleScreenMixin extends Screen {
 
     QuickConnectButtonConfig config = AutoConfig.getConfigHolder(QuickConnectButtonConfig.class).getConfig();
 
-    protected TitleScreenMixin(Text title) {
+    protected TitleScreenMixin(Component title) {
         super(title);
     }
 
-    @Inject(method = "initWidgetsNormal", at = @At(value = "TAIL"))
-    private void addCustomButton(int y, int spacingY, CallbackInfo ci) {
+    @Inject(method = "createNormalMenuOptions", at = @At(value = "TAIL"))
+    private void addCustomButton(int i, int j, CallbackInfoReturnable<Integer> cir) {
         int offsetX = 0;
-
-        Text text = Text.literal(config.connectButton);
-        int width = Math.max(config.dimensions.width, textRenderer.getWidth(text) + 20);
+        Component text = Component.literal(config.connectButton);
+        int width = config.dimensions.width;
 
         switch (config.buttonAlign) {
             case LEFT -> offsetX = -128 - (width / 2);
             case RIGHT -> offsetX = 105;
         }
 
-        ServerInfo info = new ServerInfo(I18n.translate("selectServer.defaultName"), config.address, false);
-
+        ServerData info = new ServerData("Custom Server", config.address, ServerData.Type.OTHER);
         switch (config.resourcePackPolicy) {
-            case ALLOW -> info.setResourcePackPolicy(ServerInfo.ResourcePackPolicy.ENABLED);
-            case DENY -> info.setResourcePackPolicy(ServerInfo.ResourcePackPolicy.DISABLED);
-            case PROMPT -> info.setResourcePackPolicy(ServerInfo.ResourcePackPolicy.PROMPT);
+            case ALLOW -> info.setResourcePackStatus(ServerData.ServerPackStatus.ENABLED);
+            case DENY -> info.setResourcePackStatus(ServerData.ServerPackStatus.DISABLED);
+            case PROMPT -> info.setResourcePackStatus(ServerData.ServerPackStatus.PROMPT);
         }
 
-        var button = new ButtonWidget.Builder(Text.literal(config.connectButton), (buttonWidget) -> {
-            ConnectScreen.connect(this, this.client, ServerAddress.parse(config.address), info);
-            buttonWidget.playDownSound(this.client.getSoundManager());
-        }).dimensions(this.width / 2 + offsetX, y, config.dimensions.width, config.dimensions.height).build();
+        var button = Button.builder(Component.literal(config.connectButton), (buttonWidget) -> {
+            ConnectScreen.startConnecting(this, this.minecraft, ServerAddress.parseString(config.address), info, false, null);
+            buttonWidget.playDownSound(this.minecraft.getSoundManager());
+        }).bounds(this.width / 2 + offsetX, i - 2*j, config.dimensions.width, config.dimensions.height).build();
 
-        this.addDrawableChild(button);
+        this.addRenderableWidget(button);
+
     }
 }
